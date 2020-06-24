46ea647cc1406d01a6878ca8536d468a312fdf77
marina556 <marinamakram556@gmail.com>
2020-06-24 18:39:09 +0200

    Updates

---
+++ precache-manifest.6e4c80b1fe35a120358c26ad130c90ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d396ac03ba87f148d57800dcd8a2542e",
+    "url": "/Todo-App-react-/index.html"
+  },
+  {
+    "revision": "8bab8e50eb5a9d7dc675",
+    "url": "/Todo-App-react-/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8fdf4d48c35c36e57137",
+    "url": "/Todo-App-react-/static/css/main.29479fc2.chunk.css"
+  },
+  {
+    "revision": "8bab8e50eb5a9d7dc675",
+    "url": "/Todo-App-react-/static/js/2.4d68266b.chunk.js"
+  },
+  {
+    "revision": "b396b9c7cb89986c0edcd31803c9a394",
+    "url": "/Todo-App-react-/static/js/2.4d68266b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fdf4d48c35c36e57137",
+    "url": "/Todo-App-react-/static/js/main.97c3b202.chunk.js"
+  },
+  {
+    "revision": "b49270e8abb809489f06",
+    "url": "/Todo-App-react-/static/js/runtime-main.997e9c54.js"
+  }
+]);
